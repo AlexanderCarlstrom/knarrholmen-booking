befc163cedddeb658a1f93cd7b209661bc606fd3
Alexander <caarlstrom.alexander@gmail.com>
2020-12-01 10:54:36 +0100

    create custom checkbox

--- frontend/src/components/Auth/LogIn.tsx
+++ frontend/src/components/Auth/LogIn.tsx
@@ -1,52 +1,21 @@
-import { withFormik, Form as FormikForm, Field } from 'formik';
+import { Form as FormikForm, Field, Formik } from 'formik';
 import { Form, Button, Checkbox, Typography } from 'antd';
 import { Link as RouterLink } from 'react-router-dom';
 import * as Yup from 'yup';
 import 'antd/dist/antd.css';
 import React from 'react';
 
-import './Auth.scss';
 import { FormInput } from '../Shared/FormInput/FormInput';
+import { publicFetch } from '../../utils/fetch';
+import './Auth.scss';
+import FormCheckbox from '../Shared/FormCheckbox/FormCheckbox';
 
 const { Text, Link } = Typography;
-
-const LogInForm = () => {
-  return (
-    <React.Fragment>
-      <h3 className="title">Log In</h3>
-      <FormikForm>
-        <Field name="email" placeholder="Email address" label="Email address" component={FormInput} />
-        <Field name="password" placeholder="Password" label="Password" inputType="password" component={FormInput} />
-        <Form.Item valuePropName="checked">
-          <Checkbox name="remember">Remember me</Checkbox>
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit" className="submit-btn" disabled={false}>
-            SIGN IN
-          </Button>
-        </Form.Item>
-      </FormikForm>
-      <div className="actions">
-        <Link>Forgot Password?</Link>
-        <Text>
-          {"Don't Have an Account?"}{' '}
-          <Link>
-            <RouterLink to="/auth/sign-up">Sign Up</RouterLink>
-          </Link>
-        </Text>
-      </div>
-    </React.Fragment>
-  );
-};
 
 type FormValues = {
   email: string;
   password: string;
   remember: boolean;
-};
-
-type LogInProps = {
-  initialEmail?: string;
 };
 
 const LogInSchema = Yup.object().shape({
@@ -58,18 +27,53 @@ const LogInSchema = Yup.object().shape({
   remember: Yup.bool(),
 });
 
-const LogIn = withFormik<LogInProps, FormValues>({
-  mapPropsToValues: (props) => {
-    return {
-      email: props.initialEmail || '',
-      password: '',
-      remember: true,
-    };
-  },
-  validationSchema: LogInSchema,
-  handleSubmit: (values) => {
-    console.log(values);
-  },
-})(LogInForm);
+const InitialValues = {
+  email: '',
+  password: '',
+  remember: true,
+};
+
+const LogIn = () => {
+  const submit = async (credentials: FormValues) => {
+    const result = await publicFetch.post('users/login', credentials);
+    console.log(result);
+  };
+
+  return (
+    <React.Fragment>
+      <h3 className="title">Log In</h3>
+      <Formik initialValues={InitialValues} onSubmit={(values) => submit(values)} validationSchema={LogInSchema}>
+        {(props) => (
+          <FormikForm>
+            <Field name="email" placeholder="Email address" label="Email address" component={FormInput} />
+            <Field name="password" placeholder="Password" label="Password" inputType="password" component={FormInput} />
+            <Field name="remember" label="Keep me logged in" component={FormCheckbox} />
+            <Form.Item>
+              <Button
+                type="primary"
+                size="large"
+                shape="round"
+                htmlType="submit"
+                className="submit-btn"
+                disabled={!props.isValid}
+              >
+                LOG IN
+              </Button>
+            </Form.Item>
+          </FormikForm>
+        )}
+      </Formik>
+      <div className="actions">
+        <Link>Forgot Password?</Link>
+        <Text>
+          {"Don't Have an Account?"}{' '}
+          <RouterLink to="/auth/sign-up" component={Link}>
+            Sign Up
+          </RouterLink>
+        </Text>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default LogIn;
--- frontend/src/utils/fetch.ts
+++ frontend/src/utils/fetch.ts
@@ -1,3 +1,3 @@
 import axios from 'axios';
 
-export const publicFetch = axios.create({ baseURL: process.env.API_URL });
+export const publicFetch = axios.create({ baseURL: process.env.REACT_APP_API_URL });
