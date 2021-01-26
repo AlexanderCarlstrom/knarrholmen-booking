ec70323549b16083830a586201a818a5593a992f
Alexander <caarlstrom.alexander@gmail.com>
2021-01-26 16:24:07 +0100

    Add refresh token interceptor

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,6 +1,26 @@
-import axios from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { UserResponse } from '../types/ApiReponse';
 
 const publicFetch = axios.create({ baseURL: process.env.REACT_APP_API_URL });
-const privateFetch = axios.create({ baseURL: process.env.REACT_APP_API_URL });
+const privateFetch = axios.create({ baseURL: process.env.REACT_APP_API_URL, withCredentials: true });
 
-export { publicFetch, privateFetch };
+const setUpAuthInterceptors = (refreshToken: () => Promise<AxiosResponse<UserResponse>>, logout: () => void) => {
+  privateFetch.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      const originalRequest = error.config;
+      if (error.response.status === 401) {
+        refreshToken()
+          .then(() => {
+            return axios(originalRequest);
+          })
+          .catch(() => {
+            logout();
+            return Promise.reject();
+          });
+      }
+    },
+  );
+};
+
+export { publicFetch, privateFetch, setUpAuthInterceptors };
